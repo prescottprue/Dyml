9396c206f699a2410241959e9233ffc21d879053
prescottprue <prescottprue@hotmail.com>
2014-03-15 16:44:56 -0400

    Merge conflict

--- development/ios/device-ios.app/assets/src/js/routes.js
+++ development/ios/device-ios.app/assets/src/js/routes.js
@@ -2,38 +2,29 @@
 
 angular.module('myApp.routes', ['ngRoute'])
 
-// configure views; the authRequired parameter is used for specifying pages
-// which should only be available while logged in
-.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.when('/home', {
-            templateUrl: 'partials/home.html',
-            controller: 'HomeCtrl'
-        });
+   // configure views; the authRequired parameter is used for specifying pages
+   // which should only be available while logged in
+   .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/home', {
+         templateUrl: 'partials/home.html',
+         controller: 'HomeCtrl'
+      });
 
-        $routeProvider.when('/chat', {
-            templateUrl: 'partials/chat.html',
-            controller: 'ChatCtrl'
-        });
+      $routeProvider.when('/chat', {
+         templateUrl: 'partials/chat.html',
+         controller: 'ChatCtrl'
+      });
 
-        $routeProvider.when('/account', {
-            authRequired: true, // must authenticate before viewing this page
-            templateUrl: 'partials/account.html',
-            controller: 'AccountCtrl'
-        });
+      $routeProvider.when('/account', {
+         authRequired: true, // must authenticate before viewing this page
+         templateUrl: 'partials/account.html',
+         controller: 'AccountCtrl'
+      });
 
-        $routeProvider.when('/login', {
-            templateUrl: 'partials/login.html',
-            controller: 'LoginCtrl'
-        });
+      $routeProvider.when('/login', {
+         templateUrl: 'partials/login.html',
+         controller: 'LoginCtrl'
+      });
 
-        $routeProvider.when('/medications', {
-            templateUrl: 'partials/medications.html',
-            controller: 'BBCtrl'
-        });
-
-        $routeProvider.otherwise({
-            redirectTo: '/home'
-        });
-    }
-]);
+      $routeProvider.otherwise({redirectTo: '/home'});
+   }]);
